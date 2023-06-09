783b0ab3059a704d5e6b701be569a468bf7e25bc
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-06-09 13:27:40 +0200

    Sec13. LayoutPage y LazyLoading-Children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+//loadChildren se usa para el lazyload
+// dominio.com/'' = dominio.com
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then (m => m.AuthModule),
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heores/heores.module').then (m => m.HeoresModule),
+  },
+  {
+    path:'404',
+    component: Error404PageComponent
+  },
+  {
+    path:'',
+    redirectTo: 'heroes',
+    pathMatch: 'full' // que se ajuste completamente al path vacio
+  },
+  {
+    path:'**', //cualquier otro path
+    redirectTo: '404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
